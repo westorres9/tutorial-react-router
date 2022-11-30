8c45a07f69ea3733d5205551b590f7e9f6aa9c3a
westorres9 <wester.torres9@gmail.com>
2022-11-30 17:27:54 -0300

    not found, route params

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,21 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
 import Expenses from './routes/Expenses'
+import Invoice from './routes/Invoice'
 import Invoices from './routes/Invoices'
+import NotFound from './routes/NotFound'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />} />
+        <Route path="invoices/" element={<Invoices />}>
+          <Route path=":invoiceId" element={<Invoice />}/>
+        </Route>
+        <Route path="*" element={<NotFound/>}/>
       </Route>
+      
     </Routes>
   </BrowserRouter>
 )
